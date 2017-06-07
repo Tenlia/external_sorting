df24063ee1a840e8e44945a491d56b0a72b39567
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2017-06-06 20:58:17 -0700

    added methods to sort files

--- src/main/java/com/codefellows/App.java
+++ src/main/java/com/codefellows/App.java
@@ -1,13 +1,19 @@
 package com.codefellows;
 
+import com.google.code.externalsorting.ExternalSort;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        File f1 = new File("input.txt");
+        File f2 = new File("output.txt");
+        ExternalSort.mergeSortedFiles(ExternalSort.sortInBatch(f1), f2);
     }
 }
